0e5456a023b177de8506c99dbfd3cf3bce4c480e
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-06-20 18:28:07 -0300

    author model

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const routerBook = require('./book.router');
+const routerAuthor = require('./author.router');
 const router = express.Router();
 
 // colocar las rutas aquí
 
-
+router.use('/books', routerBook)
+router.use('/authors', routerAuthor)
 
 module.exports = router;
